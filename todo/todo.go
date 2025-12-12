4cdb3b9cba3262e8da1aff9982f8335a7d345264
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-12-12 00:39:14 -0300

    .

--- todo/todo.go
+++ todo/todo.go
@@ -6,10 +6,10 @@ type Todo struct {
 	Text string `json:"text"`
 }
 
-func New(text string) Todo {
+func New(text string) (Todo, error) {
 	return Todo{
 		Text: text,
-	}
+	}, nil
 }
 
 func (todo Todo) Display() {
